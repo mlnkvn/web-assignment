d5c653c216191c3e6c39319004bef4f00c48cbdf
mlnkvn <olimpiada.nadya@yandex.ru>
2023-07-13 10:22:56 +0300

    header bug fixed(hope so)

--- header.php
+++ header.php
@@ -2,6 +2,9 @@
 <html>
 
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>WebAssignment</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
@@ -12,7 +15,6 @@
     <div class="top-bar">
 		<div class="container">
 			<div class="col-12 text-right">
-				<!-- TODO  -->
 			</div>
 		</div> 
 	</div>
